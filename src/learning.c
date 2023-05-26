20451f962744f38728179b1d31cfc3bc22bce767
chustei <chustei@student.42berlin.de>
2023-05-26 13:19:32 +0200

    Lexer almost done, return error when the quotes are not closed

--- src/learning.c
+++ src/learning.c
@@ -6,7 +6,7 @@
 /*   By: chustei <chustei@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:39:55 by chustei           #+#    #+#             */
-/*   Updated: 2023/05/25 16:39:23 by chustei          ###   ########.fr       */
+/*   Updated: 2023/05/26 13:16:57 by chustei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,36 @@ t_minishell	*create_struct(void)
 }
 
 
-t_token	*create_token(int length, char *string, char *type)
+t_token	*create_token(int len, char *str, char *type)
 {
 	t_token	*new_token;
 
 	new_token = (t_token *)malloc(sizeof(t_token));
-	new_token->len = length;
-	new_token->str = ft_strdup(string);
+	new_token->len = len;
+	new_token->str = ft_strdup(str);
 	new_token->type = ft_strdup(type);
 	new_token->next = NULL;
 	return (new_token);
+}
+
+int	check_closed_quotes(char **arr)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	while (arr[i])
+	{
+		len = ft_strlen(arr[i]) - 1;
+		if (arr[i][0] == 34 || arr[i][len] == 34)
+			if (arr[i][0] != arr[i][len] || len == 0)
+				return (0);
+		if (arr[i][0] == 39 || arr[i][len] == 39)
+			if (arr[i][0] != arr[i][len] || len == 0)
+				return (0);
+		i++;
+	}
+	return (1);
 }
 
 void	append_token(t_minishell *shell, char **args, int i)
@@ -40,7 +60,8 @@ void	append_token(t_minishell *shell, char **args, int i)
 	t_token	*new_token;
 	t_token	*current;
 
-	new_token = create_token(ft_strlen(args[i]), args[i], "test");
+	new_token = create_token(ft_strlen(args[i]), args[i], "word");
+	printf("ARG: {%s}\n", new_token->str);
 	if (shell->head == NULL)
 		shell->head = new_token;
 	else
@@ -51,8 +72,14 @@ void	append_token(t_minishell *shell, char **args, int i)
 		current->next = new_token;
 	}
 	if (args[i + 1])
-		printf("test\n");
-
+	{
+		new_token = create_token(1, " ", "sep");
+		printf("SEP: [ ]\n");
+		current = shell->head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_token;
+	}
 }
 
 // 1. Exits the minishell on CTRL-D
@@ -113,7 +140,6 @@ int	ft_tokens(t_token *lst)
 	return (i);
 }
 
-
 void	ft_lexer(t_minishell *shell, char *input)
 {
 	char	**args;
@@ -121,13 +147,18 @@ void	ft_lexer(t_minishell *shell, char *input)
 
 	i = 0;
 	args = ft_split(input, ' ');
+	if (!(check_closed_quotes(args)))
+	{
+		printf("Error: Missing closed qoutes.\n");
+		return ;
+	}
 	while (args[i])
 	{
 		append_token(shell, args, i);
 		i++;
 	}
 	ft_printf("size of list:%i\n", ft_tokens(shell->head));
-	call_method(args);
+	// call_method(args);
 	free(args);
 }
 
