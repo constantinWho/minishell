592bc1628cfd9362474f50cdc6e0383257979e3e
chustei <chustei@student.42berlin.de>
2023-07-18 19:14:33 +0200

    shell->groups update

--- src/parser.c
+++ src/parser.c
@@ -6,11 +6,23 @@
 /*   By: chustei <chustei@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:12:42 by chustei           #+#    #+#             */
-/*   Updated: 2023/07/18 16:54:02 by chustei          ###   ########.fr       */
+/*   Updated: 2023/07/18 19:12:21 by chustei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+void	print_group(t_group *head)
+{
+	t_group	*current;
+
+	current = head;
+	while (current != NULL)
+	{
+		printf("CMD: %s\n", current->cmd);
+		current = current->next;
+	}
+}
 
 void	parser(t_minishell *shell)
 {
@@ -22,4 +34,6 @@ void	parser(t_minishell *shell)
 	if (!tokens)
 		return ;
 	add_group(tokens, &groups);
+	shell->groups = groups;
+	print_group(shell->groups);
 }
